add7139f7af4c8d0a1d436d582eab930acce533f
wswmsword <cjzjzzz@gmail.com>
2022-06-29 04:51:56 +0800

    添加最新页面的无限滚动功能，迁移 ts 文件

--- src/components/media-pin-layout/index.tsx
+++ src/components/media-pin-layout/index.tsx
@@ -1,10 +1,22 @@
 import { useState, useEffect, memo, useMemo } from "react";
-import PinLayout from "../pin-layout";
+import PinLayout, { PinProps } from "../pin-layout";
 import withAutoColumn from "../hoc/with-auto-column";
 import useMobile from "../../hooks/useMobile";
 
+interface MediaPinProps extends PinProps {
+  mGap: number,
+  gap: number,
+  mItemW: number,
+  itemW: number,
+}
+
+interface ExtraInfo {
+  colNum: number;
+  itemWidth: number;
+}
+
 /**砖块布局移动桌面适配组件 */
-const MediaPin = ({ mGap, gap, mItemW, itemW, ...rest }) => {
+const MediaPin = ({ mGap, gap, mItemW, itemW, ...rest }: Omit<MediaPinProps, keyof ExtraInfo>) => {
   const isMobile = useMobile();
   const [g, setG] = useState(gap);
   const [iW, setIW] = useState(itemW);
--- src/components/pages/latest/index.tsx
+++ src/components/pages/latest/index.tsx
@@ -1,28 +1,30 @@
 import styles from "./index.module.css";
 import useMobile from "../../../hooks/useMobile";
-import { MemoizedMediaPin } from "../../media-pin-layout";
+import MediaPin, { /* MemoizedMediaPin */ } from "../../media-pin-layout";
 import GreyPinItem from "../../fakes/grey-pin-item";
 import { random, genIdChars } from "../../../tools/number";
 import withExpandableForPin from "../../hoc/with-expandable-for-pin";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { getRandomId as getRandomEventId } from "../event/images";
 import BtnBorder from "../../btn-border";
+import withInfinitScrolling from "../../../components/hoc/with-infinit-scrolling/index";
+type latestItem = {
+  id: string,
+  h: number,
+  event: boolean,
+  eventImgId?: number,
+}
 /**最新 */
 const Latest = () => {
   const isMobile = useMobile();
-  
-  // const memoItemsData = useMemo(() => [...Array(25)].map((_, i) => ({ id: genIdChars(), h: random(69, 361) })), []);
-  const [itemsData, setData] = useState([...Array(25)].map((_, i) => ({
-    id: genIdChars(),
-    h: random(69, 361),
-    event: random(0, 1) === 0,
-    eventImgId: getRandomEventId(),
-  })));
+  const hasMounted = useRef(false); // 是否第一次加载完数据
+  const [disabledIS, setDisabledIS] = useState(false); // 禁止无限滚动
+  const [itemsData, setData] = useState<latestItem[]>([]);
   const OutletWithExpandableForPin = useMemo(() => withExpandableForPin(Outlet), []);
 
   const loadMore = useCallback(() => {
-    const nextData = [...Array(25)].map((_, i) => ({
+    const nextData = [...Array(25)].map((_, i): latestItem => ({
       id: genIdChars(),
       h: random(69, 361),
       event: random(0, 1) === 0,
@@ -30,12 +32,42 @@ const Latest = () => {
     }));
     setData(v => v.concat(nextData));
   }, []);
+  const nextPage = useCallback(() => {
+    if (! hasMounted.current) { return ; }
+    loadMore();
+    console.log('ehrh >>');
+  }, []);
+  const MediaPinWithInfinitScrolling = useMemo(() => withInfinitScrolling(MediaPin, {
+    onReachBottom: nextPage,
+  }), []);
+
+  // 首次加载数据
+  useEffect(() => {
+    loadMore();
+    setTimeout(() => {
+      hasMounted.current = true;
+    }, 100);
+  }, []);
+
+  const diableIS = useCallback(() => {
+    setDisabledIS(v => !v);
+  }, []);
+
   return <>
     <div className={`${styles.content} ${isMobile ? styles.mobile : ''}`}>
       <h1>最新</h1>
       <p>最新：最新最新最新最新最新最新最新最新最新最新最新最新最新最新最新发现。Get Back. Let It Go. Real Love.</p>
     </div>
-    <MemoizedMediaPin
+    <div style={{
+      position: 'fixed',
+      zIndex: 3,
+      left: '18px',
+      bottom: '5px',
+    }}>
+      <BtnBorder onClick={diableIS}>{disabledIS ? '开启无限滚动' : '禁止无限滚动'}</BtnBorder>
+    </div>
+    <MediaPinWithInfinitScrolling
+      disabledIS={disabledIS}
       mGap={12}
       gap={36}
       mItemW={180}
@@ -44,14 +76,14 @@ const Latest = () => {
       ItemComp={GreyPinItem}
       PlaceComp={OutletWithExpandableForPin}
       placeHeight={521} />
-    <div style={{
+    {disabledIS && <div style={{
       height: "99px",
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
     }}>
       <BtnBorder onClick={loadMore}>More</BtnBorder>
-    </div>
+    </div>}
   </>;
 };
 
