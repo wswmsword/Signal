5accf8f408c2f39e58d3de9a1b3d6f17b95d3697
wswmsword <cjzjzzz@gmail.com>
2022-09-19 14:48:25 +0800

    添加路由的动态加载，添加错误边界组件

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@
 import "./App.css";
 import NavBar from "./components/nav-bar";
 import DraftsDrawer from "./components/drafts-drawer";
-import Collection from "./components/pages/collection";
-import Latest from "./components/pages/latest";
-import Rooms from "./components/pages/rooms";
 import { Routes, Route, useLocation } from "react-router-dom";
 import preval from "preval.macro";
 import GreyPinPlace from "./components/fakes/grey-pin-place";
-import NoMatch from "./components/pages/404";
-import Event from "./components/pages/event";
+import { lazy, Suspense } from "react";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+const Collection = lazy(() => import("./components/pages/collection"));
+const Latest = lazy(() => import("./components/pages/latest"));
+const Rooms = lazy(() => import("./components/pages/rooms"));
+const NoMatch = lazy(() => import("./components/pages/404"));
+const Event = lazy(() => import("./components/pages/event"));
 
 function App() {
   const location = useLocation();
@@ -17,36 +20,41 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes location={state?.backgroundLocation || location}>
-        <Route path="collections">
-          <Route path=":id" element={<Collection />}>
-            <Route path="msgs">
-              <Route path=":msgId" element={<GreyPinPlace />} />
+      <ErrorBoundary>
+        <Suspense fallback={<div>loading ...</div>}>
+          <Routes location={state?.backgroundLocation || location}>
+            <Route path="collections">
+              <Route path=":id" element={<Collection />}>
+                <Route path="msgs">
+                  <Route path=":msgId" element={<GreyPinPlace />} />
+                </Route>
+              </Route>
             </Route>
-          </Route>
-        </Route>
-        <Route path="rooms" element={<Rooms />} />
-        <Route path="" element={<Latest />}>
-          <Route path="msgs">
-            <Route path=":msgId" element={<GreyPinPlace />} />
-          </Route>
-        </Route>
-        <Route path="*" element={<NoMatch />} />
-        <Route path="/events/:id" element={<Event />}>
-          <Route path="msgs">
-            <Route path=":msgId" element={<GreyPinPlace />} />
-          </Route>
-        </Route>
-      </Routes>
-      {state?.backgroundLocation && (
-        <Routes>
-          <Route path="/events/:id" element={<Event />}>
-            <Route path="msgs">
-              <Route path=":msgId" element={<GreyPinPlace />} />
+            <Route path="rooms" element={<Rooms />} />
+            <Route path="" element={<Latest />}>
+              <Route path="msgs">
+                <Route path=":msgId" element={<GreyPinPlace />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      )}
+            <Route path="*" element={<NoMatch />} />
+            <Route path="/events/:id" element={<Event />}>
+              <Route path="msgs">
+                <Route path=":msgId" element={<GreyPinPlace />} />
+              </Route>
+            </Route>
+          </Routes>
+        
+          {state?.backgroundLocation && (
+            <Routes>
+              <Route path="/events/:id" element={<Event />}>
+                <Route path="msgs">
+                  <Route path=":msgId" element={<GreyPinPlace />} />
+                </Route>
+              </Route>
+            </Routes>
+          )}
+        </Suspense>
+      </ErrorBoundary>
       <DraftsDrawer />
       <p>
         最后 build 时间: {preval`module.exports = new Date().toLocaleString();`}.
